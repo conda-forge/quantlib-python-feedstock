8babdc0c066b7b5ea76b4e55ae5486fe97434297
Isuru Fernando <isuruf@gmail.com>
2024-11-11 11:48:33 -0800

    test after installing

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,10 @@
 ./autogen.sh
 ./configure PYTHON CXXFLAGS='-O1'
 make -C Python
+make -C Python wheel
+pip install Python/dist/QuantLib-*.whl
+
+# testing
 pushd Python
 pytest
 popd
-make -C Python wheel
-pip install Python/dist/QuantLib-*.whl
